b07b922831650ac44993f7529793385404018212
natu <natu@localhost.localdomain>
2019-09-07 02:34:43 +0200

    first commit

---
+++ 001A.js
@@ -0,0 +1,5 @@
+function button0(){return 0;}function button1(){return 1;}function button2(){return 2;}function button3(){return 3;}function button4(){return 4;}
+function button5(){return 5;}
+function buttonClicked(pointer){var elem1 = document.getElementsByClassName('menu')[0];if(pointer == 0){dat = ['foto1.png', 'texto1', 'foto2.png', 'texto2'];
+ elem1.innerHTML+=""}else if(pointer == 1){}else if(pointer == 2){}else if(pointer == 3){}else if(pointer == 4){}
+else if(pointer == 5){}}
